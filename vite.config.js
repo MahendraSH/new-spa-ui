2b6a364d57290de92ccd1c941978c945f7d958d3
MahendraSH <maahi2001sh@gmail.com>
2024-04-28 09:11:55 +0530

    proxy is not working

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
 import path from "path";
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const url = env.VITE_APP_API_URL;
-  return {
-
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-
-    server: {
-      proxy: { "/api": url },
-
-    },
-  }
+  },
 });
