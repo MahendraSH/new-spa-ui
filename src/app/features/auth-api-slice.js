2735944c481040d1772a5fd4d7493fc1a643cd98
MahendraSH <maahi2001sh@gmail.com>
2024-04-28 09:10:43 +0530

    proxy is not working

--- src/app/features/auth-api-slice.js
+++ src/app/features/auth-api-slice.js
@@ -1,10 +1,10 @@
 //  auth api slice 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// const api = import.meta.env.VITE_APP_API_URL;
+const api = import.meta.env.VITE_APP_API_URL;
 
 const authApiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "/api/auth"
+        baseUrl: api + "/api/auth"
 
     }),
     reducerPath: "authApi",
